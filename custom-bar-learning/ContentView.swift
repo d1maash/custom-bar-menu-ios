e9b84c524c18dd0cf1abdb318fb9b03e8a53d73e
Dimash <dimashaijarikov@gmail.com>
2024-08-09 21:22:14 +0500

    Initial Commit

---
+++ custom-bar-learning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  custom-bar-learning
+//
+//  Created by Динмуханбет Айжарыков on 09.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ custom-bar-learning/custom_bar_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  custom_bar_learningApp.swift
+//  custom-bar-learning
+//
+//  Created by Динмуханбет Айжарыков on 09.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct custom_bar_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
